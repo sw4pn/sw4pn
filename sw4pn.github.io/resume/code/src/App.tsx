641f3f79bc6d94beca04192bf23a47a0cf08e37a
Swapnil* <sw4pn@yahoo.com>
2023-06-17 12:55:03 +0530

    init

---
+++ sw4pn.github.io/resume/code/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <div className="text-2xl font-black  w-screen h-screen fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        UNDER CONSTRUCTION
+      </div>
+      {/* sidebar */}
+    </>
+  );
+}
+
+export default App;
---
+++ sw4pn.github.io/resume/code/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        arvo: "Arvo, serif",
+        quick: "Quicksand, sans-serif",
+        robo: "Ruboto, sans-serif",
+        rubik: "Rubik, sans-serif",
+      },
+    },
+  },
+  plugins: [],
+};
